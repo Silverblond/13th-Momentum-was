d50ae3e1706d023982326d05936b985a0f46af81
subin <han04245@naver.com>
2025-08-12 22:39:26 +0900

    start

---
+++ src/test/java/backstudy/heatcaution/HeatCautionApplicationTests.java
@@ -0,0 +1,13 @@
+package backstudy.heatcaution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatCautionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
