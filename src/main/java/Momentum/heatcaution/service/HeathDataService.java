82497ffbdbb7c4d1c67a99b81315a0e82dd505af
silverblond <kdg23538@yu.ac.kr>
2025-08-20 21:33:02 +0900

    feat: HealthDataService 구현

---
+++ src/main/java/Momentum/heatcaution/service/HeathDataService.java
@@ -0,0 +1,32 @@
+package Momentum.heatcaution.service;
+
+import Momentum.heatcaution.dto.HealthDataRequest;
+import Momentum.heatcaution.entity.HealthData;
+import Momentum.heatcaution.entity.User;
+import Momentum.heatcaution.exception.UserNotFoundException;
+import Momentum.heatcaution.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class HeathDataService {
+    private final HealthDataRepository healthDataRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void saveHealthData(String username, HealthDataRequest request) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException());
+
+        HealthData healthData = HealthData.builder()
+                .user(user)
+                .measurementTime(request.measurementTime())
+                .heartRate(request.heartRate())
+                .bodyTemperature(request.bodyTemperature())
+                .build();
+
+        healthDataRepository.save(healthData);
+    }
+}
