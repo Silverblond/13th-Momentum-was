5334ea292df0b66251f3562a4bd327c10fdf8670
subin <han04245@naver.com>
2025-08-22 22:42:54 +0900

    fix: CORS config 중괄호 오류

--- src/main/java/Momentum/heatcaution/config/CorsConfig.java
+++ src/main/java/Momentum/heatcaution/config/CorsConfig.java
@@ -15,5 +15,5 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보(쿠키) 허용
-
+    }
 }
