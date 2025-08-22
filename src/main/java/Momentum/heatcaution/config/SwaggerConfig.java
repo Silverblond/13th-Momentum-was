c8486dea70b61feae3dc4690cb987401f6a906ef
subin <han04245@naver.com>
2025-08-22 17:52:24 +0900

    fix: CORS config 설정

--- src/main/java/Momentum/heatcaution/config/SwaggerConfig.java
+++ src/main/java/Momentum/heatcaution/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package Momentum.heatcaution.SwaggerConfig;
+package Momentum.heatcaution.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "Ondobi API", version = "1.0", description = "Ondobi API Documentation"),
+        info = @Info(title = "Ondobi API", version = "1.0"),
         servers = {
-                @Server(url = "https://ondobi.site", description = "Production Server"), // URL 수정
+                @Server(url = "https://ondobi.site", description = "Production Server"),
                 @Server(url = "http://localhost:8080", description = "Local Server")
         }
 )
