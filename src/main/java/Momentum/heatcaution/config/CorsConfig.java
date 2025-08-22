016055457056078ab597968db921cdb92fa238e6
subin <han04245@naver.com>
2025-08-23 02:08:18 +0900

    fix: cors 수정

--- src/main/java/Momentum/heatcaution/config/CorsConfig.java
+++ src/main/java/Momentum/heatcaution/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보(쿠키) 허용
